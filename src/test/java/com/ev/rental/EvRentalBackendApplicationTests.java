73e92a54ab0cf94c394cee5d9dacc8ac4bfaff21
phuctan153 <phuctan153@gmail.com>
2025-10-01 10:37:11 +0700

    Init Project

---
+++ src/test/java/com/ev/rental/EvRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvRentalBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

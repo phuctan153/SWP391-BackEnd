d5e12ee9a82f4082cf404e17adbd929b9f0d58dd
lamhoanghai-dang <justinbiahoi123@gmail.com>
2025-10-21 15:57:26 +0700

    hop-dong

--- src/main/java/com/example/ev_rental_backend/config/SwaggerConfig.java
+++ src/main/java/com/example/ev_rental_backend/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +15,19 @@ public class SwaggerConfig {
 //    OpenAPI JSON: http://localhost:8080/v3/api-docs
     @Bean
     public OpenAPI apiInfo() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Nhập token JWT (ví dụ: Bearer eyJhbGciOiJIUzI1NiIs...)")
+                        )
+                )
                 .info(new Info()
                         .title("EV Rental API Documentation")
                         .description("Tài liệu API cho hệ thống thuê xe điện (EV Rental System)")

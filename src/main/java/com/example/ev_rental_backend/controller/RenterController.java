0b60b540d6b7ae14e4da69303e1431d0cfb14963
lamhoanghai-dang <justinbiahoi123@gmail.com>
2025-10-28 11:35:24 +0700

    update profile

--- src/main/java/com/example/ev_rental_backend/controller/RenterController.java
+++ src/main/java/com/example/ev_rental_backend/controller/RenterController.java
@@ -61,6 +61,7 @@ public class RenterController {
 
             // 🔹 Map sang DTO phản hồi
             RenterResponseDTO responseDTO = renterMapper.toResponseDto(renter);
+            responseDTO.setKycStatus(renterService.getKycStatusForRenter(renter));
 
             return ResponseEntity.ok(ApiResponse.builder()
                     .status("success")

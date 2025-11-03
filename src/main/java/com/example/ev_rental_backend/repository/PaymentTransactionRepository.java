c675b298197d4678b79d71976aa1b7b05396e2e2
phuctan153 <phuctan153@gmail.com>
2025-11-03 11:10:53 +0700

    Update

--- src/main/java/com/example/ev_rental_backend/repository/PaymentTransactionRepository.java
+++ src/main/java/com/example/ev_rental_backend/repository/PaymentTransactionRepository.java
@@ -4,9 +4,11 @@ import com.example.ev_rental_backend.entity.PaymentTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PaymentTransactionRepository extends JpaRepository<PaymentTransaction, Long> {
     Optional<PaymentTransaction> findByOrderCode(Long orderCode);
+    List<PaymentTransaction> findByWallet_WalletId(Long walletId);
 }
